98b5fa3ba13a99f74ad9193730ec658729f2f407
Luis Echegaray <mrluisechegaray@gmail.com>
2015-08-19 16:44:35 -0400

    first jenkins commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
